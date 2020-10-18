3595754202eaa4f04ead1a05ce2bcd3cfbd443f5
ihsandroid1 <maenbos30@gmail.com>
2020-10-19 00:07:50 +0700

    regis satu

--- src/pages/registrasi/index.js
+++ src/pages/registrasi/index.js
@@ -1,19 +1,85 @@
 import React from 'react';
 
 //material ui
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography'
+import TextField from  '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 
 //import style
 import useStyles from './style';
 
+//react router dom
+import { Link } from 'react-router-dom';
+
 function Registrasi(){
     
     const classes = useStyles();
 
-    return <>
-    <h1 className={classes.blue}>Halaman Registrasi</h1>
-    <Button color="primary" variant="contained" >Klik Disini</Button>
-    </>
+    return <Container maxWidth="xs">
+        <Paper className={classes.paper}>
+            <Typography 
+            variant="h5"
+            component="h1"
+            className={classes.title}>Buat Akun Baru
+            </Typography>
+           
+            <form>
+                <TextField
+                    id="email"
+                    type="email"
+                    name="email"
+                    margin="normal"
+                    label="Alamat Email"
+                    fullWidth
+                    required
+                />
+                <TextField
+                    id="password"
+                    type="password"
+                    name="password"
+                    margin="normal"
+                    label="Password"
+                    fullWidth
+                    required
+                />
+                 <TextField
+                    id="ulangi_password"
+                    type="password"
+                    name="ulangi_password"
+                    margin="normal"
+                    label="Ulangi Password"
+                    fullWidth
+                    required
+                />
+
+                <Grid container className={classes.buttons}>
+                    <Grid item xs>
+                    <Button 
+                    type="submit"
+                    color="primary" 
+                    variant="contained"
+                    size="large" 
+                    >Daftar</Button>
+                    </Grid>
+
+                    <Grid item>
+                    <Button
+                    component={Link}
+                    variant="contained"
+                    size="large"
+                    to="/login"
+                    >Login</Button>
+                    </Grid>
+                </Grid>
+                    
+            </form>
+
+            
+        </Paper>
+    </Container>
 }
 
 export default Registrasi;
--- src/pages/registrasi/style.js
+++ src/pages/registrasi/style.js
@@ -2,9 +2,20 @@ import {makeStyles} from '@material-ui/styles';
 
 
 const useStyles = makeStyles(theme=> ({
-    blue: {
-        color: theme.palette.primary.main
+    title: {
+        textAlign: theme.center,
+        marginBottom: theme.spacing(3)
+    },
+
+    paper: {
+        marginTop: theme.spacing(8),
+        padding: theme.spacing(6)
+    },
+
+    buttons: {
+        marginTop: theme.spacing(6)
     }
+
 })) 
 
 export default useStyles;
